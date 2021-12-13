bf6ba7356dfa45dc6116a9ed35f2a25d65df9a00
ansike <984315765@qq.com>
2021-12-13 22:22:28 +0800

    shell

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-
-node -v
-
 yarn build
 
+mkdir docs
+
+cp ./dist/* docs/
+
+git branch -D gh-pages
+
+git checkout -b gh-pages
+
+git add .
+
+git commit -m "chore"
+
+git push origin :gh-pages
+
+git push origin gh-pages
+
+git checkout master
